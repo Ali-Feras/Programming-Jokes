7d43693a17cab8c57ff3cbf757c08bea9ea0d6a2
Ali Feras <ferasali876@gmail.com>
2022-07-26 10:51:01 -0500

    Initial Commit

---
+++ Programming Jokes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Programming Jokes
+//
+//  Created by Ali on 26/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
